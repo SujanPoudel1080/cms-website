1d9d6b2d2c39a3882eb852bb17363922a3973720
SujanPoudel1080 <sujanpoudel1080@gmail.com>
2023-10-03 15:08:04 +0545

    changes in folder structure

---
+++ client/src/routes/routes.jsx
@@ -0,0 +1,74 @@
+import { createBrowserRouter } from "react-router-dom";
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+  AddJob,
+  AllJobs,
+  Profile,
+  Admin,
+  Stats,
+} from "../pages";
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/dashboard",
+        element: <DashboardLayout />,
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        element: <AddJob />,
+      },
+      { path: "stats", element: <Stats /> },
+      {
+        path: "all-jobs",
+        element: <AllJobs />,
+      },
+
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "admin",
+        element: <Admin />,
+      },
+    ],
+  },
+
+  {
+    path: "/error",
+    element: <Error />,
+  },
+  {
+    path: "/landing",
+    element: <Landing />,
+  },
+]);
+
+export default routes;
